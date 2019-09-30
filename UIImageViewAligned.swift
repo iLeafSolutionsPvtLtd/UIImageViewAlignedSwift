493eb01ec44d6beb56c1bf80363c4e7ced6ab13c
Vivek <vivekilleaf@viveks-mac-mini.local>
2019-09-30 19:18:35 +0530

    updated super image

--- UIImageViewAligned.swift
+++ UIImageViewAligned.swift
@@ -239,7 +239,7 @@ open class UIImageViewAligned: UIImageView {
         // Make sure we clear the contents of this container layer, since it refreshes from the image property once in a while.
         layer.contents = nil
         if #available(iOS 11, *) {
-            super.image = nil
+            super.image = UIImage(named: "transparent.png")
         }
     }
     
